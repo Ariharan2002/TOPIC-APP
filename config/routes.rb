a3f61fb299b4f563de5cab396e1e8b816c708c30
Ariharan S <arishanmu@gmail.com>
2023-02-19 22:04:53 +0530

    Added Comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts
   end
+  resources :posts do 
+    resources :comments
+  end
+    
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
